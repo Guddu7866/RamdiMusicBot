64a1412b51eecb3672b231332f8106c813156492
Mr Lucky <94621036+Mrvk1703@users.noreply.github.com>
2022-04-07 06:19:58 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousR1025/RamdiMusicBot",
+    "https://github.com/mrvk1703/RamdiMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
